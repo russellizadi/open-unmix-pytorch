c319a8ffe043cee41b659f42ed10594990c717a5
russell <mizadi@nd.edu>
2020-01-17 17:53:09 -0500

    update train, eval, train_eval

--- eval.sh
+++ eval.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-# train and eval
-export CUDA_VISIBLE_DEVICES=3
+# choose the cuda id
+export CUDA_VISIBLE_DEVICES=0
 echo CUDA ID: $CUDA_VISIBLE_DEVICES
 
 # set args
+path_datasets="/home/russell/russellizadi/datasets"
 dataset="musdb"
-
 name_dataset="musdb18_debug"
-#name_dataset="musdb18_debug"
-name_model="OpenUnmixHidden"
-#name_model="OpenUnmixMixup"
-#name_model="OpenUnmix"
-
-#tag_model="umx_101"
-#tag_model="musdb_musdb18_wav_OpenUnmixHidden_1023"
-tag_model="musdbn_musdb18_debug_OpenUnmixHidden_1026"
-#tag_model="must"
-
+epochs="1"
 targets="vocals"
 #targets="vocals drums bass other"
-subset="test"
-num_cores="1"
-no_cuda="0"
-is_wav="1"
+samples_per_track="64"
+is_wav="--is-wav"
 
 # don't change
-path_dataset="/home/russell/russellizadi/datasets/$dataset/$name_dataset"
-path_model="models/$tag_model"
-path_pred="preds/$tag_model/$name_dataset"
-path_eval="evals/$tag_model/$name_dataset"
+tag_model="${dataset}_${name_dataset}_${epochs}"
 
+# eval
+root="${path_datasets}/${dataset}/${name_dataset}"
+output="models/${tag_model}"
+model=$output
+outdir="preds/$tag_model/$name_dataset"
+evaldir="evals/$tag_model/$name_dataset"
+subset="test"
 
-if [ $no_cuda -eq 1 -a $is_wav -eq 1 ]; then
-	python eval.py --targets $targets --model $path_model --outdir $path_pred --evaldir $path_eval --root $path_dataset --subset $subset --cores $num_cores --no-cude --is-wav
-elif [ $no_cuda -eq 1 -a $is_wav -eq 0 ]; then
-	python eval.py --targets $targets --model $path_model --outdir $path_pred --evaldir $path_eval --root $path_dataset --subset $subset --cores $num_cores --no-cude
-elif [ $no_cuda -eq 0 -a $is_wav -eq 1 ]; then
-	python eval.py --targets $targets --model $path_model --outdir $path_pred --evaldir $path_eval --root $path_dataset --subset $subset --cores $num_cores --is-wav
-else
-	python eval.py --targets $targets --model $path_model --outdir $path_pred --evaldir $path_eval --root $path_dataset --subset $subset --cores $num_cores
-fi
+python eval.py --model ${model} --targets ${targets} --root ${root} --subset $subset --outdir $outdir --evaldir $evaldir --is-wav 
--- train.sh
+++ train.sh
@@ -5,24 +5,22 @@ export CUDA_VISIBLE_DEVICES=0
 echo CUDA ID: $CUDA_VISIBLE_DEVICES
 
 # set args
+path_datasets="/home/russell/russellizadi/datasets"
 dataset="musdb"
-
 name_dataset="musdb18_debug"
-
-name_model="OpenUnmixHidden"
-#name_model="OpenUnmixMixup"
-
-tag_model="umh_epoch10"
-
-epochs="10"
+epochs="1"
 targets="vocals"
 #targets="vocals drums bass other"
-
-# train
-root="/home/russell/russellizadi/datasets/$dataset/$name_dataset"
-output="models/$tag_model"
+samples_per_track="64"
 is_wav="--is-wav"
 
-for target in $targets; do
-	python train.py --dataset $dataset --root=$root --name-model $name_model --epochs $epochs --output=$output --target=$target --is-wav
+# don't change
+tag_model="${dataset}_${name_dataset}_${epochs}"
+
+# train
+root="${path_datasets}/${dataset}/${name_dataset}"
+output="models/${tag_model}"
+
+for target in ${targets}; do
+	python train.py --dataset ${dataset} --root=${root} --epochs ${epochs} --output=${output} --samples-per-track ${samples_per_track} --target=${target} --is-wav
 done
